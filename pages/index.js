63126c349a85a4f19e9648ae2bbc871b0608b19d
Víctor Andrés Carrillo Barreto <victorcarrillox2@gmail.com>
2025-10-06 09:59:17 -0400

    Migracion a Next.js - E-commerce Rodaindustrial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>¡Bienvenido a Rodaindustrial!</h1>
+      <p>Proyecto en desarrollo...</p>
+      <p>Fecha: {new Date().toLocaleString('es-ES')}</p>
+    </div>
+  );
+}
