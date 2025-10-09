f61ba94df633db81aa0f0f1882e65fd586770435
Víctor Andrés Carrillo Barreto <victorcarrillox2@gmail.com>
2025-10-09 13:11:41 -0400

    Listo, tuve que borrar pages/auth/_app.js, mantuve, styles/globals.css, __app.js

--- pages/auth/_app.js
+++
@@ -1,6 +0,0 @@
-// pages/_app.js - Configuración global de Next.js
-import '../styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
